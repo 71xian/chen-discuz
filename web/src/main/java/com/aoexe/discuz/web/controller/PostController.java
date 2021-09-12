fd53d9da46d047e8ac4afaa6156539594cf7e817
陈育先 <2586846075@qq.com>
2021-09-12 21:02:07 +0800

    save

--- web/src/main/java/com/aoexe/discuz/web/controller/PostController.java
+++ web/src/main/java/com/aoexe/discuz/web/controller/PostController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 
-@RestController
+@RestController /// test
 @RequestMapping("/post")
 @Api(value = "帖子管理", tags = "帖子管理")
 public class PostController {
